4635f629e1f9a1da0ef6507ee3219bda39e1d23b
chaque1111 <99604927+chaque1111@users.noreply.github.com>
2023-01-26 22:07:35 -0300

    modificando ruta de back

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import Cookies from "universal-cookie";
-const localhost = "http://localhost:8080";
+const localhost = "https://servidor-preventistas.onrender.com/";
 axios.defaults.baseURL = localhost;
 const cookies = new Cookies();
 export function getAllSellers() {
